c50db98e1a0717907e21ffa6bdee05332e629c1c
Mohammad Al osta <mohammad.alosta@sky.uk>
2023-11-01 16:31:40 +0000

    init commit

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -1,1 +1,10 @@
 // add your database connection here
+const {Sequelize, Module, DataTypes} = require("sequelize");
+const path = require("path");
+
+const db = new Sequelize({
+    dialect: "sqlite",
+    storage: path.join(__dirname, "db.sqlite")
+})
+
+module.exports = {db, DataTypes};
